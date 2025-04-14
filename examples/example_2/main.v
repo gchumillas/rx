1a3260a53d24b0e04df61d3078539473a2c6f66c
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-04-14 23:34:47 +0100

    remove dom.v file and move it to an external lib

--- examples/example_2/main.v
+++ examples/example_2/main.v
@@ -1,26 +1,27 @@
 module main
 
 import rx
+import html
 
 // A simple counter component
-fn counter_component() &rx.HTMLElement {
+fn counter_component() &html.HTMLElement {
 	mut ctx := rx.context()
 	mut count := ctx.signal(0)
 
-	mut span := rx.create_element('span')
+	mut span := html.create_element('span')
 	span.set_inner_text('${count.get()}')
 
 	ctx.create_effect(fn [count, mut span]() {
 		span.set_inner_text('${count.get()}')
 	})
 
-	mut button := rx.create_element('button')
+	mut button := html.create_element('button')
 	button.set_inner_text('Increment')
 	button.add_event_listener('click', fn [mut count]() {
 		count.set(count.get() + 1)
 	})
 
-	mut div := rx.create_element('div')
+	mut div := html.create_element('div')
 	div.append_child(span)
 	div.append_child(button)
 
@@ -28,6 +29,6 @@ fn counter_component() &rx.HTMLElement {
 }
 
 fn main() {
-	mut app := rx.get_element_by_id('app')?
+	mut app := html.get_element_by_id('app')?
 	app.render(counter_component)
 }
